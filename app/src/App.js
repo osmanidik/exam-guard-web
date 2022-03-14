e1882db6c46b9b0da0ca9b53652c9e76c9c69eb5
Furkan Dolaşık <furkandol0640@gmail.com>
2022-03-14 23:07:55 +0300

    web page created

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Dialog } from '../../node_modules/primereact/dialog';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
