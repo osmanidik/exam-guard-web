16d7c20bf0ad6461b2e4d995d539dd71ca16377c
Furkan Dolaşık <furkandol0640@gmail.com>
2022-03-17 21:59:43 +0300

    login page created.

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import { Dialog } from '../../node_modules/primereact/dialog';
+import "primereact/resources/themes/vela-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="App" style={{width: "%100", height: "%100"}}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='/' exact element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ app/src/components/Login.js
@@ -0,0 +1,28 @@
+import '../App.css';
+import React, { Component } from 'react';
+import { Button } from 'primereact/button';
+
+export default class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ""
+        };
+    }
+
+
+    render() {
+        return (
+        <div className='login' style={{backgroundColor: "#fffafa"}}>
+            <div className="card buttonLogin">
+                <h3 style={{color: "black"}}>Welcome!</h3>
+                <Button className='buttonClick' label="Click to login with Google" />
+            </div>
+        </div>
+
+        )
+    }
+}
+                 
